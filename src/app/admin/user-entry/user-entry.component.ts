8a37073266add5f2e12a3d74bf671022651e446f
msaikiran <saikiran.madishetti@trakitnow.com>
2019-11-02 21:20:43 +0530

    added list,view for users

--- src/app/admin/user-entry/user-entry.component.ts
+++ src/app/admin/user-entry/user-entry.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import 'sweetalert2/src/sweetalert2.scss';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-user-entry',
@@ -10,6 +12,22 @@ export class UserEntryComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  confirmAlert() {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'Once deleted, you will not be able to recover this imaginary file!',
+      type: 'warning',
+      showCloseButton: true,
+      showCancelButton: true
+    }).then((willDelete) => {
+      if (willDelete.dismiss) {
+        Swal.fire('', 'Your imaginary file is safe!', 'error');
+      } else {
+        Swal.fire('', 'Poof! Your imaginary file has been deleted!', 'success');
+      }
+    });
   }
 
 }
--- src/app/admin/user-entry/user-entry.module.ts
+++ src/app/admin/user-entry/user-entry.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { UserEntryRoutingModule } from './user-entry-routing.module';
 import { UserEntryComponent } from './user-entry.component';
+import { SharedModule } from 'src/app/theme/shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { DataTablesModule } from 'angular-datatables';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [UserEntryComponent],
   imports: [
     CommonModule,
-    UserEntryRoutingModule
+    UserEntryRoutingModule,
+    SharedModule,
+    FormsModule,
+    DataTablesModule,
+    NgbModule
   ]
 })
 export class UserEntryModule { }
