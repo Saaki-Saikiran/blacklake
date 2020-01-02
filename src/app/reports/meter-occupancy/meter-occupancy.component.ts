923c8768b92053873891c82c5d59c0280c6411b4
Vijju <sudaganivijay@gmail.com>
2020-01-02 22:27:42 +0530

    First push Gangadhar

--- src/app/reports/meter-occupancy/meter-occupancy.component.ts
+++ src/app/reports/meter-occupancy/meter-occupancy.component.ts
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meter-occupancy.component.scss']
 })
 export class MeterOccupancyComponent implements OnInit {
-
+  dtExportButtonOptions: any = {};
+  dtColumnsReorderOptions: any = {};
+  dtResponsiveOptions: any = {};
+  dtRowSelectOptions: any = {};
+  dtRouterLinkOptions: any = {};
   constructor() { }
 
   ngOnInit() {
+    this.dtExportButtonOptions = {
+      ajax: 'fake-data/datatable-data.json',
+      columns: [{
+        title: 'Name',
+        data: 'name'
+      }, {
+        title: 'Position',
+        data: 'position'
+      }, {
+        title: 'Office',
+        data: 'office'
+      }, {
+        title: 'Age',
+        data: 'age'
+      }, {
+        title: 'Start Date',
+        data: 'date'
+      }, {
+        title: 'Salary',
+        data: 'salary'
+      }],
+      dom: 'Bfrtip',
+      buttons: [
+        'copy',
+        'print',
+        'excel',
+        'csv'
+      ]
+    };
   }
 
 }
+
--- src/app/reports/meter-occupancy/meter-occupancy.module.ts
+++ src/app/reports/meter-occupancy/meter-occupancy.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { MeterOccupancyRoutingModule } from './meter-occupancy-routing.module';
 import { MeterOccupancyComponent } from './meter-occupancy.component';
-
+import { SharedModule } from '../../theme/shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { DataTablesModule } from 'angular-datatables';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [MeterOccupancyComponent],
   imports: [
     CommonModule,
-    MeterOccupancyRoutingModule
+    MeterOccupancyRoutingModule,
+    SharedModule,
+    FormsModule,
+    DataTablesModule,
+    NgbModule
   ]
 })
 export class MeterOccupancyModule { }
